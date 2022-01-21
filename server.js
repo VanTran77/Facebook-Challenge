b15c6eb0886a9ea2f874d6d71bc9c78a4ec31ea6
Van Tran <tavan.it@gmail.com>
2022-01-21 18:43:59 +0100

    controller folder error upload

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ app.use(express.static('public'))
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 
-// require mongoose
 require('./config/mongoose')
 
-// require router
 app.use(router)
 
 app.listen(1112, ()=> console.log("Listen to port 1112..."))
